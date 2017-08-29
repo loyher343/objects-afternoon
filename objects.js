2267c99261eda0b530e6b80cdbb78728ef6555e6
Loy Hernandez <loyhernandez59@gmail.com>
2017-08-29 11:02:20 -0500

    not done

--- objects.js
+++ objects.js
@@ -2,42 +2,58 @@
 
 // Code Here
 
+var user = {
+    name : 'loy',
+    age  : '23',
+    password : 'password',
 
+};
 
 // Add an email and username, as well as a method called alertUser that alerts the users name, to your user object using dot notation.
 
 // Code Here
-
+user.email = 'email@email.com';
+user.alertUser = alert(user.name);
 
 
 // Now loop over your user object and alert each of the keys.
 
 // Code Here
+for(var key in user){
+    alert(user.key)
+}    
 
 
 
 // Now loop over your user object and alert each of the values for each key.
 
 // Code Here
-
+for(var key in user){
+    alert(user[key])
+}
 
 
 // Remove the username from your user object
 
 // Code Here
-
+delete user.name;
 
 
 // Create an object called creditCard that has number, cvc, type, and limit properties and assign them appropriate values.
 
 // Code Here
-
+var creditCard = {
+    number: 12345434543,
+    cvc   :  545,
+    type  : "master",
+    limit :  12
+}
 
 
 // Create a variable called alternating. Set it equal to the string bonusCategories.
 
 // Code Here
-
+var alternating = 'bonusCategories';
 
 
 /* Using bracket notation, add a bonusCategories key to your creditCard object using 'alternating'.
@@ -52,13 +68,16 @@
     */
 
 // Code Here
-
+    creditCard[alternating] = [obj{key1:val1,key2:val2}, obj1{key3:val3,key4:val4}]
+    creditCard.bank = {name:'the Wells',address: '1233 somewhere st'}
 
 
 // Write a function that will take your creditCard object and user object as arguments and combine them into one object. /* HINT: You will need a for..in loop */
 
 // Code Here
-
+function merge(obj1,obj2){
+    return obj = Object.assign(obj1,obj2)
+}
 
 /*Below you are given an object. Write a function that will loop over the allies property, remove LittleFinger, and insert Dragon Lady.
     It will also change alive to 'More Than Ever'.
@@ -72,4 +91,8 @@ var obj = {
 
   // Code Here
 
-  
+  function (obj){
+    for(var i = 0; i < array.length; i++){
+        if(obj[i][allies] )
+    }
+  }
